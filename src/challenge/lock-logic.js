2e9f0ca4898f0361bc342ab032a9017b2bfe2195
hamadou diallo <hamadou_diallo@yahoo.com>
2021-02-10 17:43:16 -0500

    solving lock logic and creating about page about myself

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -1,13 +1,13 @@
-const redirect = window.redirect
+const redirect = window.redirect;
 
-const SECRET_COMBO = [1, 3, 5, 1]
+const SECRET_COMBO = [1, 3, 5, 1];
 
 const lockState = window.mobx.observable({
   locked: true,
-  wheels: [0, 0, 0, 0]
-})
+  wheels: [0, 0, 0, 0],
+});
 
-function changeDialValue (index, incrementBy) {
+function changeDialValue(index, incrementBy) {
   // This part is missing some code
   // This function is automatically called when the user clicks on a chevron
   // it will be called with a wheel index and an amount to change the value by
@@ -25,8 +25,29 @@ function changeDialValue (index, incrementBy) {
   // call the redirect() function with your name
   // eg: redirect('larry-lobster')
   // the redirect function will only redirect if the lockState is unlocked
+
+  lockState.wheels[index] += incrementBy;
+  const check_validity_of_combo = (entered_combo, secret_combo) => {
+    let p1 = 0,
+      p2 = 0;
+    while (p1 < entered_combo.length && p2 < secret_combo.length) {
+      let lock_num = entered_combo[p1];
+      let secret_code_num = secret_combo[p2];
+      if (lock_num !== secret_code_num) {
+        return false;
+      }
+      p1++;
+      p2++;
+    }
+    return true;
+  };
+
+  if (check_validity_of_combo(lockState.wheels, SECRET_COMBO)) {
+    lockState.locked = false;
+    redirect("tidiane-diallo");
+  }
 }
 
 // let our other modules find our functions
-window.lockState = lockState
-window.changeDialValue = changeDialValue
+window.lockState = lockState;
+window.changeDialValue = changeDialValue;
